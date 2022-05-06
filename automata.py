2b6b08771f723a63a7e7b71f574eb19f5d046ac8
ldcas-uc3m <100429021@alumnos.uc3m.es>
2022-05-06 15:59:04 +0200

    make like 80% of the automata

--- automata.py
+++ automata.py
@@ -1,9 +1,10 @@
-from mdp import PROBABILITIES_PATH
+from mdp import POLICIES_PATH, ACTIONS
 
+import json
 import time
 
 
-CYCLE = 20
+CYCLE = 20  # time between decisions
 
 
 def generateInput():
@@ -13,18 +14,50 @@ def generateInput():
     state = ""
     hour = 0
 
-    pass
+    # TODO
 
     return state, hour
 
 
+def decide(state, hour, data):
+    """
+    Given an input state and hour, and the optimal policies data from the MPD,
+    decides the best action.
+    In case state is LLL (undefined, as it's goal state), it cycles through the
+    actions.
+    """
+    global i
+
+    if state == "LLL":  # do an action on a cycle
+        action = ACTIONS[i]
+
+        # update cycle
+        i += 1
+        if i >= len(ACTIONS):
+            i = 0
+    else:
+        action = data[hour][state]
+
+    return action
+
+
 def run():
     """
-    
+    Run the automata. Outputs to the console the action for the generated data.
     """
+    
+    # load data from MDP model
+    data = json.loads(POLICIES_PATH)
+
+    i = 0  # count current cycle of rotating lights
     while True:
         state, hour = generateInput()
+        action = decide(state, hour, data)
 
-        pass
+        print("Action:", action)
 
         time.sleep(CYCLE)
+
+
+if __name__ == "__main__":
+    run()
